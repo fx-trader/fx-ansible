fcaf034dc7645610c5d522003f72adef0e2bc42e
João Costa <joaocosta@zonalivre.org>
2018-01-27 16:58:02 +0000

    Add daily volatility column

--- roles/fx.dataaccounts/files/0000-baseschema.sql
+++ roles/fx.dataaccounts/files/0000-baseschema.sql
@@ -1,7 +1,8 @@
-CREATE TABLE stats (
+CREATE TABLE account_stats (
   `account_id`  int NOT NULL,
   `datetime`    datetime NOT NULL,
   `nav`         decimal(9,2) NOT NULL,
   `leverage`    decimal(5,2) NOT NULL,
+  `daily_volatility` decimal(7,2) NOT NULL,
   PRIMARY KEY (`account_id`, `datetime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;

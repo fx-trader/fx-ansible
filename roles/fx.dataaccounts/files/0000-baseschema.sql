aa76d2335feecd5e6f4dc0e318c55339787c3bc8
João Costa <joaocosta@zonalivre.org>
2019-08-03 23:12:43 +0000

    Remove dataaccounts

--- roles/fx.dataaccounts/files/0000-baseschema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE account_stats (
-  `account_id`  int NOT NULL,
-  `datetime`    datetime NOT NULL,
-  `nav`         decimal(9,2) NOT NULL,
-  `leverage`    decimal(5,2) NOT NULL,
-  `daily_volatility` decimal(7,2) NOT NULL,
-  PRIMARY KEY (`account_id`, `datetime`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
